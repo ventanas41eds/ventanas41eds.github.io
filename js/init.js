1fff5217de92e3c1d33baa2a645d1cbf5e1898f7
ventanas41eds <ventanas41eds@gmail.com>
2023-04-09 20:44:43 -0600

    cambio

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+const firebaseConfig = {
+    apiKey: "AIzaSyD9LrY-Ne3TRpffWXZUf3qFLmQ2vUeuVgY",
+    authDomain: "ventanas41eds-5e38a.firebaseapp.com",
+    projectId: "ventanas41eds-5e38a",
+    storageBucket: "ventanas41eds-5e38a.appspot.com",
+    messagingSenderId: "659376434451",
+    appId: "1:659376434451:web:7dd6c59310c6f43cdebee6",
+    measurementId: "G-3JSPEHFWFT"
+  };
